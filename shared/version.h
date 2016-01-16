8ab10cadeaddabd7ec5dad2c4ec1c49014c8f620
Brandon <tricksterguy@live.com>
2016-01-15 22:32:54 -0800

    Bump version

--- shared/version.h
+++ shared/version.h
@@ -2,32 +2,8 @@
 #define VERSION_H
 
 namespace AutoVersion{
-
-	//Date Version Types
-	static const char DATE[] = "21";
-	static const char MONTH[] = "11";
-	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] = "15.11";
-
-	//Software Status
-	static const char STATUS[] = "Alpha";
-	static const char STATUS_SHORT[] = "a";
-
-	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 2;
-	static const long BUILD = 0;
-	static const long REVISION = 0;
-
-	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 0;
-	#define RC_FILEVERSION 1,2,0,0
-	#define RC_FILEVERSION_STRING "1, 2, 0, 0\0"
-	static const char FULLVERSION_STRING[] = "1.2.0.0";
-
-	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 0;
-
-
+	static const long MINOR = 3;
+	static const char FULLVERSION_STRING[] = "1.3.0.0";
 }
 #endif //VERSION_H

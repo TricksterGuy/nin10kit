943690326198b40e63dc95822b796c09182d8586
Brandon <tricksterguy@live.com>
2018-11-19 18:57:53 -0800

    Fix error when compiling with -pedantic for lut generators, apparently doesn't like bitwise expressions which overflow into the sign bit

--- shared/lutgen.cpp
+++ shared/lutgen.cpp
@@ -59,10 +59,12 @@ std::string form_fixed_string(int64_t fixed, const LutIOType& lut_type)
     std::stringstream str;
     int pad = (lut_type.GetLength() + 3) / 4;
     if (pad == 0) pad = 1;
-    uint64_t mask = (1L << lut_type.GetLength()) - 1;
+    //uint64_t mask = (1L << lut_type.GetLength()) - 1;
     if (lut_type.GetLength() > 0)
-        str << "((" << (fixed >> lut_type.GetLength()) << " << " << lut_type.GetLength() << ") | 0x" << std::setfill('0')
-            << std::setw(pad) << std::hex << (fixed & mask) << ")";
+        str << "0x" << std::setfill('0') << std::setw(pad) << std::hex << fixed;
+    // Causes a bug with -pedantic turned on if the number is negative.
+    //    str << "((" << (fixed >> lut_type.GetLength()) << " << " << lut_type.GetLength() << ") | 0x" << std::setfill('0')
+    //        << std::setw(pad) << std::hex << (fixed & mask) << ")";
     else
         str << fixed;
     return str.str();
